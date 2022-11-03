bd0a7980d2dd0019655d722847fb43accee6d8e5
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-11-03 20:28:05 +0200

    deleted localStorage func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,12 +49,6 @@ export class App extends Component {
     );
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
   render() {
     const { filter } = this.state;
     const filteredList = this.filteredContacts();
